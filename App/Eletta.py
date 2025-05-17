77a5f012fe24c2497b8bdfc1e71fab10ebd527ef
User <giovannimateus11@gmail.com>
2025-05-17 09:33:47 -0300

    Algumas personalizações de tela

--- App/Eletta.py
+++ App/Eletta.py
@@ -7,15 +7,43 @@ from views import votante, host
 # ----------------- views padrões -----------------
 def pagina_inicial(page: ft.Page, controlador: 'Controlador') -> ft.View:
     conteudo_da_pagina = [
-        ft.Row(
+        ft.Column(
             [
-                ft.ElevatedButton("virar votante", on_click=controlador.entrar_na_votacao_como_votante),
-                ft.ElevatedButton("virar host", on_click=controlador.entrar_na_votacao_como_host)
+                ft.ElevatedButton(
+                    text="virar votante",
+                    width=117,
+                    height=56,
+                    color=ft.colors.WHITE,
+                    bgcolor= "#39746F",
+                    on_click=controlador.entrar_na_votacao_como_votante,
+                    style=ft.ButtonStyle(
+                        padding = 20,
+                        text_style=ft.TextStyle(size=13, weight=ft.FontWeight.NORMAL, font_family='Inter')
+                    )
+                    ),
+                ft.ElevatedButton(
+                    text="virar host",
+                    width=117,
+                    height=56,
+                    color=ft.colors.WHITE,
+                    bgcolor= "#39746F", 
+                    on_click=controlador.entrar_na_votacao_como_host,
+                    style=ft.ButtonStyle(
+                        padding = 20,
+                        text_style=ft.TextStyle(size=13, weight=ft.FontWeight.NORMAL, font_family='Inter')
+                    )
+                    )
             ],
             alignment = ft.MainAxisAlignment.CENTER
         )
     ]
-    return ft.View('/', controls=conteudo_da_pagina, vertical_alignment = ft.MainAxisAlignment.CENTER, horizontal_alignment = ft.CrossAxisAlignment.CENTER)
+    return ft.View(
+        '/', 
+        controls=conteudo_da_pagina, 
+        vertical_alignment = ft.MainAxisAlignment.CENTER, 
+        horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+        bgcolor= ft.colors.WHITE
+        )
 
 def pagina_do_resultado(page: ft.Page, resultado: str) -> ft.View:
     conteudo_da_pagina = [
--- App/views/votante.py
+++ App/views/votante.py
@@ -8,17 +8,77 @@ def pagina_de_espera(page: ft.Page) -> ft.View:
 def pagina_de_votacao(page: ft.Page, controlador: 'Controlador') -> ft.View:
     pauta = controlador.mensagem
     conteudo_da_pagina = [
-        ft.Column(
-            [
-                ft.Text(value=pauta),
-                ft.ElevatedButton('A favor', on_click=controlador.votar, data=2),
-                ft.ElevatedButton('Contra', on_click=controlador.votar, data=1),
-                ft.ElevatedButton('Abster-se', on_click=controlador.votar, data=0)
-            ],
-            alignment = ft.MainAxisAlignment.CENTER
+        
+        ft.Container(
+            content = ft.Text(
+                value=pauta,
+                size = 14,
+                text_align = ft.TextAlign.CENTER,
+                color=ft.colors.BLACK
+            ),
+            padding=20,
+            border=ft.border.all(3, "#39746F"),
+            width = 329,
+            height = 73
+
         ),
+
+        ft.Column(
+            controls=[
+
+                ft.ElevatedButton(
+                    text='A favor',
+                    width=117,
+                    height=56,
+                    bgcolor='#47D147',
+                    color=ft.colors.WHITE,
+                    on_click=controlador.votar,
+                    data=2,
+                    style=ft.ButtonStyle(
+                        padding = 20,
+                        text_style=ft.TextStyle(size=14, weight=ft.FontWeight.NORMAL, font_family='Inter')
+                    )
+                ),
+
+                ft.ElevatedButton(
+                    text='Contra',
+                    width=117,
+                    height=56,
+                    bgcolor='#C83A3A',
+                    color=ft.colors.WHITE,
+                    on_click=controlador.votar,
+                    data=1,
+                    style=ft.ButtonStyle(
+                        padding = 20,
+                        text_style=ft.TextStyle(size=14, weight=ft.FontWeight.NORMAL, font_family='Inter')
+                    )
+                ),
+
+                ft.ElevatedButton(
+                    text='Abster-se',
+                    width=117,
+                    height=56,
+                    bgcolor='#828E82',
+                    color=ft.colors.WHITE,
+                    on_click=controlador.votar,
+                    data=0,
+                    style=ft.ButtonStyle(
+                        padding = 20,
+                        text_style=ft.TextStyle(size=14, weight=ft.FontWeight.NORMAL, font_family='Inter')
+                    )
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            spacing=20
+        )
     ]
-    return ft.View('/votacao', controls=conteudo_da_pagina, vertical_alignment = ft.MainAxisAlignment.CENTER, horizontal_alignment = ft.CrossAxisAlignment.CENTER)
+    return ft.View(
+        '/votacao', 
+        controls=conteudo_da_pagina, 
+        vertical_alignment = ft.MainAxisAlignment.CENTER, 
+        horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+        bgcolor = ft.colors.WHITE
+        )
 
 def pagina_do_resultado(page: ft.Page, resultado: str) -> ft.View:
     conteudo_da_pagina = [
